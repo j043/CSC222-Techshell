8ccdb91c4e2735499a333bb416fc0982f5d15a49
j043 <165920619+j043@users.noreply.github.com>
2025-02-18 19:12:27 +0000

    techshell.c
    updated parseinput function
    PROBLEMS: not properly allocating memory, not properly inserting args into args array

--- techshell.c
+++ techshell.c
@@ -1,5 +1,5 @@
 /*
-* Name: Jenny Wright, Tia Ransom and Ashton Harrell
+* Name: Jenny Wright, Tia Ransom, and Ashton Harrell
 * Date: 2/13/25
 * Description: basic shell implementation with I/O redirection 
 */
@@ -10,9 +10,9 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <wait.h>
+#include <string.h>
 
 struct ShellCommand{
-    char commandname[50];
     char *args[10];
     int argcount;
 };
@@ -24,13 +24,40 @@ char* CommandPrompt(){ // display working directory and take user input
     char* input = malloc(100); // allocate memory to prevent segmentation fault 
     char* cwd = getcwd(NULL, 0);
     printf("%s$ ", cwd); // print pwd
-    scanf("%s", input); // take user input
+    fgets(input, 100, stdin); // take user input
     return input;
 }
 
-struct ShellCommand ParseCommandLine(char* input);
+struct ShellCommand ParseCommandLine(char* input) {
+    struct ShellCommand command; // create a ShellCommand object to return
+    // while reading user input, add individual args to args array 
+    // use counter to navigate args array
+    // use strtok() to split the input string by whitespace
 
-void ExecuteCommand(struct ShellCommand command);
+    // allocate mem for token
+    char* token = malloc(100);
+    // append tokens to *args until NULL pointer
+    for (int i = 0; i < strlen(input); i++) {
+        token = strtok(input, " ");
+        command.args[i] = token;
+    }
+    // DEBUG
+    for (int i = 0; i < 10; i++) {
+        printf("arg %d is %s", i, command.args[i]);
+    }
+    return command;
+}
+
+void ExecuteCommand(struct ShellCommand command) {
+    // if first arg of input is not a real function or not one of our defined functions, 
+    // print "function : command not found"
+    if (strcmp(command.args[0], "ls") == 0) {
+        printf("TESTING!\n");
+    }
+    else {
+        printf("%s : command not found\n", command.args[0]);
+    }
+}
 
 // MAIN
 int main(){
@@ -39,9 +66,8 @@ int main(){
     // repeatedly prompt the user for input 
     for (;;){
         input = CommandPrompt();
-        break;
-        // parse the command line 
-        //command = ParseCommandLine(input);
+        // parse the command line
+        command = ParseCommandLine(input);
         // execute the command 
         //ExecuteCommand(command);
     }
